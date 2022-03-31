51bdc33d30d847b98221634f43c8297384bd8475
Franco Beratz <francoberatz.fb@gmail.com>
2022-03-31 16:27:17 -0300

    Updates

---
+++ precache-manifest.8e5739e3a273fb3009bbc8d8c7f6eb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5dc9d9e8f840e9d60ce41b04ed1463e",
+    "url": "/tweet-simulator/index.html"
+  },
+  {
+    "revision": "bd5ea869697d7acbf054",
+    "url": "/tweet-simulator/static/css/main.d460fd5a.chunk.css"
+  },
+  {
+    "revision": "a7b42ceade9ea2822c2c",
+    "url": "/tweet-simulator/static/js/2.30092655.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tweet-simulator/static/js/2.30092655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5ea869697d7acbf054",
+    "url": "/tweet-simulator/static/js/main.f6ef1265.chunk.js"
+  },
+  {
+    "revision": "eda31b304e050f4ee183",
+    "url": "/tweet-simulator/static/js/runtime-main.5bf90d1c.js"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/tweet-simulator/static/media/original.400dc885.png"
+  }
+]);
